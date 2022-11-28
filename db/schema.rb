a0749d25fb9b957c90496044fe1f159e5416fb57
manyuel <manuelsancheza@gmail.com>
2022-11-28 16:52:23 +0000

    add cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -14,25 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_163111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.string "item"
-    t.text "description"
-    t.float "price"
-    t.boolean "is_sold"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "transactions", force: :cascade do |t|
-    t.date "date"
-    t.boolean "is_delivered"
-    t.bigint "user_id", null: false
-    t.bigint "product_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_transactions_on_product_id"
-    t.index ["user_id"], name: "index_transactions_on_user_id"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,7 +40,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_163111) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
+  create_table "products", force: :cascade do |t|
+    t.string "item"
+    t.text "description"
+    t.float "price"
+    t.boolean "is_sold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.date "date"
+    t.boolean "is_delivered"
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_transactions_on_product_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -76,9 +76,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_163111) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "transactions", "products"
   add_foreign_key "transactions", "users"
   add_foreign_key "users", "products"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
