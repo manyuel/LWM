d4c5976732c4170205f71a90fc092c170c05286a
Ammar Amin <ammaramin78@gmail.com>
2022-11-30 14:11:18 +0000

    basket

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'pages#user_dash', as: 'dashboard'
   get 'about', to: 'pages#about_us', as: 'aboutpage'
   get 'basket', to: 'transactions#basket', as: 'basket'
+  resources :transactions, only: :destroy
 
 end
