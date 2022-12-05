4752f291b5a0207ca2c3cb44d8f66d212a336f2c
Ammar Amin <ammaramin78@gmail.com>
2022-12-05 17:46:35 +0000

    stripe api

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -14,6 +14,29 @@ class TransactionsController < ApplicationController
 
   def basket
     @transactions = Transaction.where(user_id: current_user.id)
+    @total_amount = @transactions.map { |transaction| transaction.product.price.round(2) }.sum.round(2)
+    @order = Order.new(user: current_user, total_amount: @total_amount)
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          unit_amount: (@total_amount * 100).to_i,
+          product_data: {
+            name: 'Your order',
+            description: 'Item',
+            images: ['https://example.com/t-shirt.png']
+          }
+        },
+        quantity: 1
+      }],
+      mode: 'payment',
+      success_url: "http://localhost:3000/checkout",
+      cancel_url: "http://localhost:3000/checkout"
+    )
+
+    @order.update(checkout_session_id: session.id)
   end
 
   def destroy
